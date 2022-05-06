e74bc4960be723f452639207641b45b9b6ccdaa3
StevenL <1012987522@qq.com>
2022-05-06 18:19:38 +0800

    feat: 新增showtime.py

---
+++ showtime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 5/6/22 18:12
+# @Author  : StevenL
+# @Email   : stevenl365404@gmail.com
+# @File    : showtime.py
+
+from alive_progress.styles import showtime
+from alive_progress.styles import show_bars
+
+showtime()
